64d8e6e34f6b38afd47ff926e5f46449b280a008
Spencer Moody <spencer.moody@outlook.com>
2020-08-14 10:08:50 -0700

    Create Flavor class and auto implemented properties

---
+++ SweetSavory/Models/Flavor.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace SweetSavory.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.Treats = new HashSet<FlavorTreat>();
+    }
+
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<FlavorTreat> Treats { get; set; }
+  }
+}
